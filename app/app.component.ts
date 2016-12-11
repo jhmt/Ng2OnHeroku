bc899131cf5bde36555c931db1d3a6ef80b1ee79
jhmt <jhashimot00@gmail.com>
2016-12-11 19:38:37 +1300

    changing message

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<p>Hello Angular 2！</p>'
+  template: '<p>Hello Angular 2 App!</p>'
 })
 export class AppComponent { }
